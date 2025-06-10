45a84d42c200a34f246793fcc712b22a1c540c00
KlimVladMaks <vkm.blog@yandex.ru>
2025-06-10 20:33:43 +0300

    Commit 4

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ fig2 = px.bar(
     df,
     x='Часть света',
     y='Медиана',
-    title='Медианное значение уровня удовлетворённости жизнью по частям света (2024)',
+    title='Медианное значение уровня удовлетворённости жизнью<br>по частям света (2024)',
     labels={'Медиана': 'Медиана', 'Часть света': 'Часть света'},
     color='Часть света',
     text='Медиана'
@@ -63,22 +63,141 @@ fig2.update_layout(
 )
 
 # ---------- Диаграмма 3 ----------
+df = pd.read_csv('dataset_3.csv')
+
+fig3 = px.line(
+    df,
+    x='Год',
+    y=['Медиана', 'Топ-10 худших', 'Топ-10 лучших'],
+    title='Динамика мирового медианного уровня удовлетворённости жизнью',
+    labels={'value': 'Уровень удовлетворённости', 'variable': 'Показатель'},
+    color_discrete_map={
+        'Медиана': 'blue',
+        'Топ-10 худших': 'red',
+        'Топ-10 лучших': 'green'
+    }
+)
+
+# ---------- Диаграмма 4 ----------
+df_population = pd.read_csv('dataset_4.csv')
+
+population_by_category = df_population.groupby('Категория')['Население'].sum().reset_index()
+
+fig4 = px.pie(
+    population_by_category,
+    values='Население',
+    names='Категория',
+    title='Число людей, проживающих в странах с уровнем<br>удовлетворённости жизнью выше/ниже медианного (2024)',
+    color='Категория',
+    color_discrete_map={
+        'Выше медианы': 'green',
+        'Ниже медианы': 'red'
+    }
+)
+
+fig4.update_traces(
+    textinfo='percent+label',
+    hovertemplate='<b>%{label}</b><br>Население: %{value:,}'
+)
+fig4.update_layout(
+    legend_title_text='Категория'
+)
+
+# ---------- Диаграмма 5 ----------
+df_growth = pd.read_csv('dataset_5.csv').head(10)
+df_growth = df_growth.sort_values('Динамика', ascending=True)
+
+fig5 = px.bar(
+    df_growth,
+    x='Динамика',
+    y='Название',
+    orientation='h',
+    title='Топ-10 стран с самым высоким ростом уровня удовлетворенности жизнью<br>с 2011 по 2024 годы',
+    color_discrete_sequence=['#1E90FF'],
+    text='Динамика'
+)
+
+fig5.update_layout(
+    showlegend=False
+)
+
+# ---------- Диаграмма 6 ----------
+df_decline = pd.read_csv('dataset_5.csv').tail(10).sort_values('Динамика', ascending=False)
+
+fig6 = px.bar(
+    df_decline,
+    x='Динамика',
+    y='Название',
+    orientation='h',
+    title='Топ-10 стран с самым большим падением уровня<br>удовлетворённости жизнью с 2011 по 2024 годы',
+    color_discrete_sequence=['#FF6347'],  # Красивый красный цвет (Tomato)
+    text='Динамика'
+)
+
+fig6.update_layout(
+    showlegend=False
+)
+
+# ---------- Диаграмма 6 ----------
+df_categories = pd.read_csv('dataset_6.csv')
+
+category_counts = df_categories['Категория'].value_counts().reset_index()
+category_counts.columns = ['Категория', 'Количество стран']
+
+color_map = {
+    'Рост >10%': '#00FF00',
+    'Рост 0-10%': '#90EE90',
+    'Падение 0-10%': '#FFA07A',
+    'Падение >10%': '#FF0000'
+}
+
+fig7 = px.pie(
+    category_counts,
+    values='Количество стран',
+    names='Категория',
+    title='Распределение стран по динамике уровня удовлетворенности жизнью',
+    color='Категория',
+    color_discrete_map=color_map
+)
+
+fig7.update_traces(
+    textinfo='percent+label+value',
+    textposition='inside',
+    hovertemplate='<b>%{label}</b><br>Количество стран: %{value}<br>Доля: %{percent}',
+    marker=dict(line=dict(color='#FFFFFF', width=1))  # Белые границы между секторами
+)
+
+fig7.update_layout(
+    uniformtext_minsize=12,
+    uniformtext_mode='hide',
+    legend_title_text='Категории динамики',
+    margin=dict(t=80, b=30, l=30, r=30)
+)
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+"""
+- Добавить главный заголовок "Уровень удовлетворённости жизнью в мире"
+- Добавить подзаголовок "(Уровень удовлетворённости жизнью оценивается по шкале от 0 до 10)"
+"""
 app.layout = dbc.Container(
     [
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        top10_table
+                        top10_table,
+                        dcc.Graph(figure=fig3),
+                        dcc.Graph(figure=fig5),
+                        dcc.Graph(figure=fig7)
                     ],
                     width=6,
                 ),
                 dbc.Col(
                     [
-                        dcc.Graph(figure=fig2)
+                        dcc.Graph(figure=fig2),
+                        dcc.Graph(figure=fig4),
+                        dcc.Graph(figure=fig6)
                     ],
                     width=6,
                 ),
